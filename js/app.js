286bd87598e755b1eb90078cd6c892f62a894d18
mattLabuguen <rmnlabuguen@addu.edu.ph>
2021-02-10 19:56:53 +0800

    New Prepartory Assessment files
    Preparatory Assessment 2 files.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,38 @@
-function add(){
-    var number = document.getElementById("number");
-    var value = parseInt(number.innerHTML);
-    number.innerHTML = value + 1;
-}
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url.toString());
+
+    res.setHeader('Content-Type', 'text/html');
+    let url = '../views/';
+
+    if (req.url == '/'){
+        url += 'index.html';
+        res.statusCode == 200;
+    }else if(req.url == '/about'){
+        url += 'about.html';
+        res.statusCode == 200;
+    }
+    else if(req.url == '/contact'){
+        url += 'contactUs.html';
+        res.statusCode == 200;
+    }else{
+        url += '404.html';
+        res.statusCode == 404;
+    }
+    fs.readFile(url, (err, data) =>{
+        if(err) {
+            console.log(err);
+            res.end();
+        }
+        else{
+            res.write(data);
+            res.end();
+        }
+    });
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening on port 3000');
+});
